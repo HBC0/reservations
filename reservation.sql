1f978c90387befbfed2456d9536782000c8cac47
hbc <huang171303@email.com>
2024-02-29 16:44:17 +0800

    更新部分表结构（添加索引）

--- reservation.sql
+++ reservation.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80035
  File Encoding         : 65001
 
- Date: 28/02/2024 10:57:18
+ Date: 29/02/2024 16:42:43
 */
 
 SET NAMES utf8mb4;
@@ -23,14 +23,11 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `hotel`;
 CREATE TABLE `hotel`  (
   `id` int NOT NULL AUTO_INCREMENT COMMENT '酒店id',
-  `createTime` datetime NULL DEFAULT NULL,
-  `updateTime` datetime NULL DEFAULT NULL,
-  `roomInventory` int NULL DEFAULT NULL COMMENT '剩余房间数量',
-  `reserved` int NULL DEFAULT NULL COMMENT '已经预订出去的房间数量。',
+  `create_time` datetime NULL DEFAULT NULL,
+  `update_time` datetime NULL DEFAULT NULL,
   `address` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '地址',
-  `date` datetime NULL DEFAULT NULL COMMENT '日期',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of hotel
@@ -43,18 +40,19 @@ DROP TABLE IF EXISTS `order`;
 CREATE TABLE `order`  (
   `id` int NOT NULL AUTO_INCREMENT COMMENT '订单id',
   `number` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '订单编号',
-  `createTime` datetime NULL DEFAULT NULL COMMENT '创建时间',
-  `updateTime` datetime NULL DEFAULT NULL COMMENT '更新时间',
-  `payment` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '支付方式',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
+  `payment_type` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '支付方式',
   `amount` decimal(10, 2) NULL DEFAULT NULL COMMENT '总额',
   `status` tinyint NULL DEFAULT NULL COMMENT '订单状态（未支付，已支付，已取消，已完成）',
-  `roomId` int NULL DEFAULT NULL COMMENT '房间id',
-  `userId` int NULL DEFAULT NULL COMMENT '用户id',
-  `startDate` datetime NULL DEFAULT NULL COMMENT '开始时间',
-  `endDate` datetime NULL DEFAULT NULL COMMENT '结束时间',
+  `room_id` int NULL DEFAULT NULL COMMENT '房间id',
+  `user_id` int NULL DEFAULT NULL COMMENT '用户id',
+  `start_date` datetime NULL DEFAULT NULL COMMENT '开始时间',
+  `end_date` datetime NULL DEFAULT NULL COMMENT '结束时间',
   `remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '备注',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `index`(`room_id` ASC, `user_id` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of order
@@ -66,12 +64,13 @@ CREATE TABLE `order`  (
 DROP TABLE IF EXISTS `picture`;
 CREATE TABLE `picture`  (
   `id` int NOT NULL AUTO_INCREMENT,
-  `createTime` datetime NULL DEFAULT NULL,
-  `updateTime` datetime NULL DEFAULT NULL,
-  `picture` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '图片',
-  `roomId` int NULL DEFAULT NULL COMMENT '房间id',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+  `create_time` datetime NULL DEFAULT NULL,
+  `update_time` datetime NULL DEFAULT NULL,
+  `picture` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '图片路径',
+  `room_id` int NULL DEFAULT NULL COMMENT '房间id',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `index_room_id`(`room_id` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of picture
@@ -82,20 +81,40 @@ CREATE TABLE `picture`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `room`;
 CREATE TABLE `room`  (
-  `id` int NOT NULL,
-  `roomNumber` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '房间号',
-  `createTime` datetime NULL DEFAULT NULL,
-  `updateTime` datetime NULL DEFAULT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `room_number` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '房间号',
+  `create_time` datetime NULL DEFAULT NULL,
+  `update_time` datetime NULL DEFAULT NULL,
   `price` decimal(10, 2) NULL DEFAULT NULL COMMENT '房间价格',
   `details` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '详细信息',
   `status` tinyint NULL DEFAULT NULL COMMENT '房间状态（是否空闲）',
   `reviews` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '评论信息',
-  `hotelId` int NULL DEFAULT NULL COMMENT '酒店id',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+  `hotel_id` int NULL DEFAULT NULL COMMENT '酒店标识',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `index_hotel_id`(`hotel_id` ASC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of room
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for room_inventory
+-- ----------------------------
+DROP TABLE IF EXISTS `room_inventory`;
+CREATE TABLE `room_inventory`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `hotel_id` int NULL DEFAULT NULL COMMENT '酒店标识',
+  `room_id` int NULL DEFAULT NULL COMMENT '房间标识',
+  `room_inventory` int NULL DEFAULT NULL COMMENT '剩余房间数量(房间总数减去不可用房间数)',
+  `room_reserved` int NULL DEFAULT NULL COMMENT '已经预订出去的房间数量。',
+  `date` datetime NULL DEFAULT NULL COMMENT '日期',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `index`(`hotel_id` ASC, `room_id` ASC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of room_inventory
 -- ----------------------------
 
 -- ----------------------------
@@ -105,18 +124,25 @@ DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users`  (
   `id` int NOT NULL AUTO_INCREMENT COMMENT '用户id',
   `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户名',
-  `password` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '密码',
-  `phoneNumber` int NULL DEFAULT NULL COMMENT '手机号',
-  `createTime` datetime NULL DEFAULT NULL,
-  `updateTime` datetime NULL DEFAULT NULL,
+  `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '密码',
+  `phone_number` int NULL DEFAULT NULL COMMENT '手机号',
+  `create_time` datetime NULL DEFAULT NULL,
+  `update_time` datetime NULL DEFAULT NULL,
   `email` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '邮箱',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `index_phontNumber`(`phone_number` ASC) USING BTREE COMMENT '唯一索引',
+  UNIQUE INDEX `index_email`(`email` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of users
 -- ----------------------------
 INSERT INTO `users` VALUES (1, 'h', '123', 123, NULL, NULL, NULL);
 INSERT INTO `users` VALUES (2, 'jack', '132', 12341234, '2024-02-28 10:47:35', '2024-02-28 10:47:35', NULL);
+INSERT INTO `users` VALUES (3, 'jack', '132', NULL, '2024-02-28 14:38:07', '2024-02-28 14:38:07', '12341234');
+INSERT INTO `users` VALUES (5, 'jack', '132', NULL, '2024-02-28 15:57:47', '2024-02-28 15:57:47', '123');
+INSERT INTO `users` VALUES (7, '1', 'c4ca4238a0b923820dcc509a6f75849b', NULL, '2024-02-29 11:59:43', '2024-02-29 11:59:43', '1');
+INSERT INTO `users` VALUES (8, '1', 'c4ca4238a0b923820dcc509a6f75849b', NULL, '2024-02-29 12:38:28', '2024-02-29 12:38:28', '1713037329@qq.com');
+INSERT INTO `users` VALUES (17, '1', 'c4ca4238a0b923820dcc509a6f75849b', NULL, '2024-02-29 15:49:24', '2024-02-29 15:49:24', '1713037329@qq.co');
 
 SET FOREIGN_KEY_CHECKS = 1;
